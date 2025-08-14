913a947c34a778c25d26e94358a6eca5a2bdef4e
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-14 16:28:03 +0530

    add student attributes

--- backend/models/student.js
+++ backend/models/student.js
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt"
+import { boolean } from "zod";
 
 const studentSchema = new mongoose.Schema(
     {
         first_name: {type: String, required: true, trim: true},
         last_name: {type: String, required: true, trim: true},
-        password: {type: String, required: true}
+        password: {type: String, required: true},
+        date_of_birth: {type: Date},
+        student_status:{type: boolean, default: true}
     }
 );
 
@@ -17,3 +20,9 @@ studentSchema.pre("save", async(next) => {
     this.password = await bcrypt.hash(this.password,salt);
     next();
 })
+
+studentSchema.methods.comparepassword = (p) => {
+    return bcrypt.compare(p, this.password);
+}
+
+export const Student = mongoose.Model("Student",studentSchema);
