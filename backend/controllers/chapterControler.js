2c96e0887b9d13c62d092b4a305368dafeb151a4
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-18 08:10:18 +0530

    create chapter

--- backend/controllers/chapterControler.js
+++ backend/controllers/chapterControler.js
@@ -10,6 +10,46 @@ const chapterRegSchema = {
 
 }
 
+const createChapter = async (req, res) => {
+    try {
+        const chapterSchema = chapterRegSchema.safeParse(req.body);
+        if (!chapterSchema) {
+            res.status(400).json({
+                "message": "Invalid inputs"
+            })
+        }
+
+        const chapter = await Chapter.create(chapter.data);
+        if (!chapter) {
+            res.status(400).json({
+                "message": "chapter creation failed"
+            })
+        }
+
+        res.status(200).json({
+            "message": "Chapter created successfully",
+            data: {
+                chapter: {
+                    chapter_id: chapter._id,
+                    chapter_name: chapter.chapter_name,
+                    chapter_notes: chapter.chapter_notes,
+                    chapter_url: chapter.chapter_url,
+                    chapter_description: chapter.chapter_description,
+                    chapter_status: chapter.chapter_status,
+                    created: chapter.createdAt,
+                    updated_at: chapter.updatedAt,
+                }
+            }
+        })
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            "message": "Internal Server Error",
+            "error": e
+        })
+    }
+}
+
 
 
 
