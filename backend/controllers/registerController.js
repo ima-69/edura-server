dd526521035608b948012de872d942ff669ee5d4
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-21 10:05:30 +0530

    crt user updt fn

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -41,3 +41,50 @@ const creteRegister = async(req,res) => {
         })
     }
 }
+
+const updateRegSchema =  z.object({
+    class_id:z.array(z.string()),
+})
+
+const updateRegister = async(req,res) => {
+
+    try {
+        const id = req.body.id;
+
+        if (!id){
+            res.status(400).json({
+
+                "message": "id (student) is required"
+
+            })
+        }
+        const registerSafeParse = createRegister.safeParse(req.body);
+        if(!registerSafeParse){
+            res.status(400).json({
+                "message": "invalid inputs"
+            })
+        }
+
+        const result = await Register.findByIdAndUpdate(id,registerSafeParse.data,{new: true});
+        if(!result){
+            res.status(404).json({
+                "message": "Register not found or update failed"
+            })
+        }
+        res.status(200).json({
+            message: "Successfully updated register",
+            data: {
+                student_id: result.student_id,
+                class_id : result.class_id ,
+            }
+        })
+    }catch(e){
+        console.log(e);
+        res.status(500).json({
+            "message": "invalid inputs",
+            "error": e.message,
+        })
+    }
+
+
+}
